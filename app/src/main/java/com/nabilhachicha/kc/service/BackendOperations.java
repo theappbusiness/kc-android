e6aff44c9d036544a5a638a9a14939b980f19217
Nabil Hachicha <nabil.hachicha@gmail.com>
2015-04-25 15:42:09 +0100

    using new endpoints

--- app/src/main/java/com/nabilhachicha/kc/service/BackendOperations.java
+++ app/src/main/java/com/nabilhachicha/kc/service/BackendOperations.java
@@ -11,13 +11,9 @@ import rx.Observable;
  * Created by Nabil on 25/04/15.
  */
 public interface BackendOperations {
-    //GET /categories
-    @GET("/553b6f49e2eb2f5d1890a11e")
+    @GET("/categories")
     Observable<CategoriesResponse> getCategories();
 
-    //553b6f82e2eb2f691890a11f
-    //GET /categories/:id/venues`
-    @GET("/{category}")
-    Observable<VenuesResponse> getVenuesByCategory(@Path("category") String category);
-//    List<POI> getVenueDetails ();
+    @GET("/categories/{id}/venues")
+    Observable<VenuesResponse> getVenuesByCategory(@Path("id") String category);
 }
