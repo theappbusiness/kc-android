ad1c17e2cc0cf21d80a4bb01d012944e85093ae1
Stephen Colias <stephen.colias@gmail.com>
2015-04-25 11:36:46 +0100

    added fragment to activity test

--- app/src/main/java/com/nabilhachicha/kc/items/itemlist/ItemsFragment.java
+++ app/src/main/java/com/nabilhachicha/kc/items/itemlist/ItemsFragment.java
@@ -2,6 +2,7 @@ package com.nabilhachicha.kc.items.itemlist;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,6 +44,10 @@ public class ItemsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.item_list);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
         // Set the adapter
         mRecyclerView.setAdapter(mAdapter);
