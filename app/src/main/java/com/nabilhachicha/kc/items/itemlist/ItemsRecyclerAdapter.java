2266ce1d5f395fe319639123283c3a43757e9b35
Stephen Colias <stephen.colias@gmail.com>
2015-04-25 13:00:31 +0100

    comments

--- app/src/main/java/com/nabilhachicha/kc/items/itemlist/ItemsRecyclerAdapter.java
+++ app/src/main/java/com/nabilhachicha/kc/items/itemlist/ItemsRecyclerAdapter.java
@@ -22,10 +22,8 @@ public class ItemsRecyclerAdapter extends RecyclerView.Adapter<ItemsRecyclerAdap
     private Picasso mPicasso;
 
     // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
+
         public ImageView mImageView;
         public TextView mTitleTextView;
         public TextView mDescriptionTextView;
@@ -38,13 +36,12 @@ public class ItemsRecyclerAdapter extends RecyclerView.Adapter<ItemsRecyclerAdap
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public ItemsRecyclerAdapter(List<POI> myDataset, Picasso picasso) {
         mDataset = myDataset;
         mPicasso = picasso;
     }
 
-    // Create new views (invoked by the layout manager)
+    // Create new views
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent,
                                          int viewType) {
@@ -58,15 +55,15 @@ public class ItemsRecyclerAdapter extends RecyclerView.Adapter<ItemsRecyclerAdap
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
+        // - get element from the dataset at this position
+        // - replace the contents of the view with this element
         POI poi = mDataset.get(position);
         holder.mTitleTextView.setText(poi.getName());
         holder.mDescriptionTextView.setText(poi.getDescription());
         mPicasso.with(holder.mImageView.getContext()).load(poi.getImgUrl()).into(holder.mImageView);
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    // Return the size of the dataset
     @Override
     public int getItemCount() {
         return mDataset.size();
