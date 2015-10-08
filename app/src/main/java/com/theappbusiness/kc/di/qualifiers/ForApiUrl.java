8a68dc91872f8d60c31fa062ccf0eceddc571991
Robotoaster <swav.kulinski@gmail.com>
2015-10-08 18:18:51 +0100

    fix copyright

--- app/src/main/java/com/theappbusiness/kc/di/qualifiers/ForApiUrl.java
+++ app/src/main/java/com/theappbusiness/kc/di/qualifiers/ForApiUrl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Smart Studio
+ * Copyright 2015 The App Business
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
